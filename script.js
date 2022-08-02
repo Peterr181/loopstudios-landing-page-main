0666232e38e0ea93650770f2eb6b57e6c61e3b06
Peterr181 <piotrzerdzinski089@gmail.com>
2022-08-02 23:30:35 +0200

    commited

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const HeaderEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  HeaderEl.classList.toggle("nav-open");
+});
